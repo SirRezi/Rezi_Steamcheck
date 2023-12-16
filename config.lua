5a7313f4c53409f7c0d92b6a683809e9036a912e
SirRezi <138231740+SirRezi@users.noreply.github.com>
2023-12-16 01:00:46 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.Webhook = "DEINE WEBHOOK"
+
+-- Embed-Einstellungen
+Config.EmbedSettings = {
+    title = "SirRezi System",
+    color = 65280, -- FARBE
+    icon_url = "DEIN BILD / NOTWENDIG!",
+    watermark = "System von SirRezi"
+}
